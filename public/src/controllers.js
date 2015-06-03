d8f5ae3fd3562927cd333207d7066dbdc0a3d83d
Manoj Fernando <mj.bitwave@gmail.com>
2015-06-03 20:01:08 +0530

    Functionality

---
+++ public/src/controllers.js
@@ -0,0 +1,14 @@
+angular.module('ContactsApp')
+    .controller('ListController', function($scope, Contact) {
+        // $scope.contacts = Contact.query();
+        $scope.contacts =Contact.query();
+        $scope.fields = ['firstName', 'lastName'];
+
+        $scope.sort = function(field) {
+            $scope.sort.field = field;
+            $scope.sort.order = !$scope.sort.order;
+        };
+
+        $scope.sort.field = 'firstName';
+        $scope.sort.order = false;
+    });
